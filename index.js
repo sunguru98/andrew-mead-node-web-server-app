a066223aa928c819bfe66dc68a2bd8eae3c8c037
sundeep <sunguru98@yahoo.co.in>
2018-09-13 18:40:39 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const hbs = require("hbs");
+const fs = require("fs");
+
+
+
+let app = express();
+
+hbs.registerPartials(__dirname+"/views/partials");
+app.use((request,response,next)=>{
+    log = `${new Date().toString()} ${request.method} ${request.url}`;
+    console.log(log);
+    fs.appendFileSync("serverMessages.log",log+"\n");
+    next();
+})
+
+app.use((req,res,next)=>{
+    res.render("maintenance.hbs");
+    
+})
+
+
+app.set("view engine","hbs");
+app.get("/",(request,response)=>{
+    response.render("home.hbs",{
+        pageTitle:"Welcome Page",
+        pageMessage:"This is the Main welcome page",
+        welcomeMessage:"We used hbs to sync with the help of the backend Node.js framework",
+        copyrightYear:new Date().getFullYear(),
+    });
+});
+
+app.get("/contact-us",(request,response)=>{
+    response.render("contact-us.hbs",{
+        pageTitle:"Contact Us page",
+        pageMessage:"This is the Contact-us page",
+        welcomeMessage:"We are here in the contact us",
+        copyrightYear:new Date().getFullYear(),
+    });   
+});
+
+app.get("/bad",(request,response)=>{
+    response.send({
+        errorMessage:"There is something wrong about the server connectivity please try again",
+    });
+    console.log(request);
+});
+
+app.listen(9998);
